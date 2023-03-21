dda7a7e8c2eef3f8cfdf9d9517aff901836574a6
João Provezi <jprovezi@gmail.com>
2023-03-21 20:59:12 -0300

    atualizado tag google

--- resources/views/templates/inovex/main.blade.php
+++ resources/views/templates/inovex/main.blade.php
@@ -209,7 +209,16 @@
                 $(this).toggleClass('maxSize')
             });
         });
-    </script>    
+    </script>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-59760875-1"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-59760875-1');
+    </script>
 </body>
 
 </html>
